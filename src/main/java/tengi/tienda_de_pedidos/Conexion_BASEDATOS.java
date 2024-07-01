9500c47c02ac873a82616c9741566912d457155b
Moises Aguilar <moises.aguilar.et7@gmail.com>
2024-06-30 22:15:44 -0300

    modificamos la conecion de base de datos

--- src/main/java/tengi/tienda_de_pedidos/Conexion_BASEDATOS.java
+++ src/main/java/tengi/tienda_de_pedidos/Conexion_BASEDATOS.java
@@ -4,19 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Conexion_BASEDATOS{
-    private static final String DB_NAME = "conector_Javafx";
-    private static final String DB_USER = "nombre_Apellido";
-    private static final String DB_PASSWORD = "contraseña";
-    private static final String DB_URL = "jdbc:mysql://localhost/" + DB_NAME;
+public class Conexion_BASEDATOS {
+    private static final String URL = "jdbc:mysql://localhost:3306/Conexion_BASEDATOS";
+    private static final String USUARIO = "root";
+    private static final String CONTRASENIA = "123456";
 
-    public static Connection getConnection() throws SQLException {
+    public Connection getConnection() {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-        } catch (ClassNotFoundException | SQLException e) {
-            throw new SQLException("Error al conectar a la base de datos", e);
-
+            return DriverManager.getConnection(URL, USUARIO, CONTRASENIA);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error al conectar con la base de datos", e);
         }
     }
 }
